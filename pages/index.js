fd83039005f67564c8c29054614c4399cd55e51a
Josephhuang921 <95013399+Josephhuang921@users.noreply.github.com>
2022-11-26 14:07:25 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Domain For Sale BYD!</title>
+        <title>Domain For Sale BYD SOLUTIONS!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
